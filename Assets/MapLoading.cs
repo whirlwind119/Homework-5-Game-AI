13ff114ce7153dcedad83fac4eeafc4ad0594274
whirlwind119 <tim.kim1996@gmail.com>
2017-11-05 22:11:42 -0500

    Added A*

--- Assets/MapLoading.cs
+++ Assets/MapLoading.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
@@ -10,6 +11,9 @@ public class MapLoading : MonoBehaviour {
     public GameObject Wall;
     public GameObject Tree;
     public GameObject Tile;
+    public Camera cam;
+    public int y_index = 3;//305
+    public int x_index = 3;//294
 
     GameObject[][] DataMap;
     GameObject[][] Tiles;
@@ -17,20 +21,25 @@ public class MapLoading : MonoBehaviour {
     GameObject[] DataRow;
     public int tileSize;
     int length;
+
+
+    float native_width = 1920;
+    float native_height = 1080;
+
     // Use this for initialization
     void Start() {
-        int roundedlength = Mathf.CeilToInt(294 * 305 / (tileSize * tileSize) + 294);
+        int roundedlength = Mathf.CeilToInt(y_index * x_index / (tileSize * tileSize) + y_index);
         length = roundedlength;
-        DataRow = new GameObject[294];
-        DataMap = new GameObject[305][];
+        DataRow = new GameObject[x_index];
+        DataMap = new GameObject[y_index][];
         TileRow = new GameObject[tileSize * tileSize];
         Tiles = new GameObject[length][];
         string line;
         int y = 0;
         bool start = false;
-        for(int i = 0; i < 305; i++)
+        for(int i = 0; i < y_index; i++)
         {
-            for(int j = 0; j < 294; j++)
+            for(int j = 0; j < x_index; j++)
             {
                 DataRow[j] = null;
             }
@@ -38,7 +47,7 @@ public class MapLoading : MonoBehaviour {
         }
         int tilecount = 0;
 
-        StreamReader reader = new StreamReader("Assets/Map.txt", Encoding.Default);
+        StreamReader reader = new StreamReader("Assets/Map2.txt", Encoding.Default);
 
         using (reader)
         {
@@ -55,18 +64,27 @@ public class MapLoading : MonoBehaviour {
                             if (line[i] == '@')
                             {
                                 type = Instantiate(Wall, new Vector2(i * (1.28f) + 0.64f, y * (1.28f) + 0.64f), Quaternion.identity);
+                                type.transform.tag = "wall";
                                 DataMap[y][i] = type;
                             }
                             else if (line[i] == '.')
                             {
                                 type = Instantiate(Path, new Vector2(i * 1.28f + 0.64f, y * 1.28f + 0.64f), Quaternion.identity);
+                                type.transform.tag = "path";
+                                type.AddComponent<BoxCollider2D>();
+                                type.AddComponent<SelectNode>();
+                                type.GetComponent<SelectNode>().x = i;
+                                type.GetComponent<SelectNode>().y = y;
+
                                 DataMap[y][i] = type;
                             }
                             else if (line[i] == 'T')
                             {
                                 type = Instantiate(Tree, new Vector2(i * 1.28f + 0.64f, y * 1.28f+ 0.64f), Quaternion.identity);
+                                type.transform.tag = "tree";
                                 DataMap[y][i] = type;
                             }
+                            //Debug.Log("y:" + y + "     x:"  + i + "     type:" + DataMap[y][i]);
                         }
                         y++;
                     }
@@ -79,6 +97,7 @@ public class MapLoading : MonoBehaviour {
             reader.Close();
 
         }
+        /*
         for (int i = 0; i < DataMap.Length; i++)
         {
             if (i % tileSize == 0)
@@ -105,17 +124,123 @@ public class MapLoading : MonoBehaviour {
                             k++;
                         }
                         Tiles[tilecount] = TileRow;
-                        Instantiate(Tile, new Vector3(j * 1.28f+ (0.64f * tileSize), i * 1.28f + (0.64f * tileSize), 1.1f), Quaternion.identity);
+                        Instantiate(Tile, new Vector3(j * 1.28f+ (0.64f * tileSize), i * 1.28f + (0.64f * tileSize), 1f), Quaternion.identity);
                         tilecount++;
                         
                     }
                 }
             }
         }
+        */
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
+
+    private List<GameObject> findPath(GameObject startNode, GameObject endNode) {
+        List<GameObject> openList = new List<GameObject>();
+
+        startNode.GetComponent<SelectNode>().g = 0;
+        startNode.GetComponent<SelectNode>().f = 0;
+
+        openList.Add(startNode);
+        startNode.GetComponent<SelectNode>().opened = true;
+
+        while(openList.Count > 0) {
+            //Debug.Log(openList.Count);
+            GameObject node = openList[0];
+            openList.Remove(openList[0]);
+            //Debug.Log(openList.Count);
+            node.GetComponent<SelectNode>().closed = true;
+
+            if (node == endNode) {
+                return composePath(startNode,endNode);
+            }
+            //Debug.Log(node);
+            //Destroy(node);
+           
+            Debug.Log(startNode.GetComponent<SelectNode>().y);
+            Debug.Log(startNode.GetComponent<SelectNode>().x);
+            Destroy(DataMap[startNode.GetComponent<SelectNode>().y][startNode.GetComponent<SelectNode>().x]);
+
+            Destroy(DataMap[node.GetComponent<SelectNode>().y][node.GetComponent<SelectNode>().x]);
+            List<GameObject> neighbors = findNeighbors(node);
+            for (int i = 0, l = neighbors.Count; i<l; ++i) {
+                GameObject neighbor = neighbors[i];
+                if (neighbor.GetComponent<SelectNode>().closed) {
+                    continue;
+                }
+
+                int x = neighbor.GetComponent<SelectNode>().x;
+                int y = neighbor.GetComponent<SelectNode>().y;
+
+                float ng = node.GetComponent<SelectNode>().g + 1;
+                if(!neighbor.GetComponent<SelectNode>().opened || ng < neighbor.GetComponent<SelectNode>().g) {
+                    neighbor.GetComponent<SelectNode>().g = ng;
+                    neighbor.GetComponent<SelectNode>().f = neighbor.GetComponent<SelectNode>().g + 1;
+                    neighbor.GetComponent<SelectNode>().parent = node;
+
+                    if (!neighbor.GetComponent<SelectNode>().opened) {
+                        openList.Add(neighbor);
+                        neighbor.GetComponent<SelectNode>().opened = true;
+                    }
+                    else {
+                        openList.Sort(sortByFScore);
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private void OnGUI() {
+        float rx = Screen.width / native_width;
+        float ry = Screen.height / native_height;
+        GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(rx, ry, 1));
+
+        if (GUI.Button(new Rect((native_width) - 200, 50, 150, 100), "Start A*")) {
+            GameObject start = GameObject.FindGameObjectWithTag("start");
+            GameObject goal = GameObject.FindGameObjectWithTag("goal");
+            findPath(start,goal);
+        }
+    }
+
+    static int sortByFScore(GameObject node1, GameObject node2) {
+        return node1.GetComponent<SelectNode>().f.CompareTo(node2.GetComponent<SelectNode>().f);
+    }
+
+    private List<GameObject> composePath(GameObject startNode, GameObject endNode) {
+        List<GameObject> path = new List<GameObject>();
+        GameObject curNode = endNode;
+        while(curNode != startNode) {
+            curNode = curNode.GetComponent<SelectNode>().parent;
+            curNode.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+            path.Add(curNode);
+        }
+        return path;
+    }
+
+    private List<GameObject> findNeighbors(GameObject currentNode) {
+        List<GameObject> neighbors = new List<GameObject>();
+        SelectNode node = currentNode.GetComponent<SelectNode>();
+        int x = node.x;
+        int y = node.y;
+
+        if (DataMap[y-1][x].gameObject.transform.tag == "path") {
+            neighbors.Add(DataMap[x - 1][y]);
+        }
+        if (DataMap[y + 1][x].gameObject.transform.tag == "path") {
+            neighbors.Add(DataMap[x - 1][y]);
+        }
+        if (DataMap[y][x-1].gameObject.transform.tag == "path") {
+            neighbors.Add(DataMap[x - 1][y]);
+        }
+        if (DataMap[y][x+1].gameObject.transform.tag == "path") {
+            neighbors.Add(DataMap[x - 1][y]);
+        }
+        return neighbors;
+    }
 }
---
+++ Assets/SelectNode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectNode : MonoBehaviour {
+
+    public int x;
+    public int y;
+    public float g;
+    public float f;
+    public bool opened = false;
+    public bool closed = false;
+    public GameObject parent = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnMouseOver() {
+        if (Input.GetMouseButtonDown(0)) {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            transform.tag = "start";
+        }
+        if (Input.GetMouseButtonDown(1)) {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            transform.tag = "goal";
+        }
+    }
+}
